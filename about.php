a675abb600504d5c84f4ad01e165f96bc1b2749d
JomariePaolo <90879374+JomariePaolo@users.noreply.github.com>
2023-02-12 22:13:26 +0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,40 @@
+   <!-- /.row -->
+          
+                     
+            <div class="row">
+                 
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3><b>  Mission and Vision</b>
+                           </h3>
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <p>
+                               <h1> VISION</h1>
+
+                                    dumugyot tuy skwela by 2025
+
+                                     
+
+                                   <h1>  MISSION</h1>
+
+                                    makapaduldog yuh
+
+                                     
+
+                                   <h1> GOALS</h1>
+                                   <ul>
+                                    <li>ukininayo amin.</li>
+                                    
+                                   </ul>
+                                </p>
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-6 -->
+            </div>
+           
